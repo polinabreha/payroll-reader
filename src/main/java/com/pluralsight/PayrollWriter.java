da781cb9c57deef5dae8823f3652f7633b641495
polinabreha <polinabreha@gmail.com>
2026-04-22 09:36:01 -0400

    writer

--- src/main/java/com/pluralsight/PayrollWriter.java
+++ src/main/java/com/pluralsight/PayrollWriter.java
@@ -12,8 +12,31 @@ public class PayrollWriter {
             System.out.print("Enter the name of the payroll file to create: " );
             String payrollFileName = input.nextLine();
 
+            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String text;
+
             FileWriter fileWriter = new FileWriter("src/main/resources/" + payrollFileName);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write("id|name|gross pay");
+            bufferedWriter.newLine();
+
+
+            while((text = bufferedReader.readLine()) != null){
+                String [] fields = text.split("\\|");
+                Employee employee = new Employee(Integer.parseInt(fields[0]),
+                        fields[1],
+                        Double.parseDouble(fields[2]),
+                        Double.parseDouble(fields[3]));
+                bufferedWriter.write(employee.getEmployeeId() + "|" + employee.getName() + "|" + employee.getGrossPay()) ;
+                bufferedWriter.newLine();
+
+
+            }
+            bufferedReader.close();
+            bufferedWriter.close();
+
 
 
 
